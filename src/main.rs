35c1dd7402fd6e76144bbe7eefd52bcf4f0e40f3
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-12-29 18:32:39 +1100

    🏯 add new simpleresponse type for improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 
 use clap::Parser;
-use reqwest::Client;
+use reqwest::{Client, Response, StatusCode};
+use serde::Serialize;
 use serde_json::Value;
 
 #[derive(Parser, Debug)]
@@ -11,29 +12,71 @@ struct Args {
 }
 
 #[derive(Debug)]
-struct Row {
+struct Column {
     id: String,
     name: String,
-    row_type: String,
+    column_type: String,
 }
 
-async fn fetch_notion_database(database_id: &str, token: &str) -> Result<String, Box<dyn Error>> {
+#[derive(Debug)]
+struct DatabaseCredentials {
+    id: String,
+    token: String,
+}
+
+#[derive(Serialize)]
+struct SimpleColumnQuery<'a> {
+    property: &'a str,
+}
+
+#[derive(Debug)]
+struct SimpleResponse {
+    status: StatusCode,
+    body: String,
+}
+
+impl SimpleResponse {
+    async fn from_response(res: Response) -> Self {
+        SimpleResponse {
+            status: res.status(),
+            body: res.text().await.unwrap(),
+        }
+    }
+    async fn from_error(err: reqwest::Error) -> Self {
+        SimpleResponse {
+            status: err.status().unwrap(),
+            body: err.to_string(),
+        }
+    }
+}
+
+async fn response_to_result(res: Response) -> Result<SimpleResponse, SimpleResponse> {
+    let status_body = SimpleResponse::from_response(res).await;
+
+    if status_body.status.is_success() {
+        Ok(status_body)
+    } else {
+        Err(status_body)
+    }
+}
+
+async fn fetch_notion_database(
+    credentials: &DatabaseCredentials,
+) -> Result<SimpleResponse, SimpleResponse> {
     let client = Client::new();
-    let url = format!("https://api.notion.com/v1/databases/{database_id}");
+    let url = format!("https://api.notion.com/v1/databases/{}", credentials.id);
 
     let response = client
         .get(url)
-        .header("Authorization", format!("Bearer {token}"))
+        .header("Authorization", format!("Bearer {}", credentials.token))
         .header("Notion-Version", "2022-06-28")
         .send()
-        .await?
-        .text()
-        .await?;
+        .await;
 
-    Ok(response)
+    response_to_result(response.unwrap()).await
 }
 
-fn get_db_rows(db: &str) -> Result<Option<Vec<Row>>, Box<dyn Error>> {
+fn get_db_columns(db: &str) -> Result<Option<Vec<Column>>, Box<dyn Error>> {
     let body: Value = serde_json::from_str(db)?;
     let properties = body.get("properties");
     if properties.is_none() {
@@ -45,20 +88,66 @@ fn get_db_rows(db: &str) -> Result<Option<Vec<Row>>, Box<dyn Error>> {
             .as_object()
             .unwrap()
             .values()
-            .map(|property| Row {
-                id: property.get("id").unwrap().to_string(),
-                name: property.get("name").unwrap().to_string(),
-                row_type: property.get("type").unwrap().to_string(),
+            .map(|property| Column {
+                id: property.get("id").unwrap().as_str().unwrap().to_string(),
+                name: property.get("name").unwrap().as_str().unwrap().to_string(),
+                column_type: property.get("type").unwrap().as_str().unwrap().to_string(),
             })
             .collect(),
     ))
 }
 
+async fn query_rows(
+    credentials: &DatabaseCredentials,
+    column: &Column,
+) -> Result<SimpleResponse, SimpleResponse> {
+    let client = Client::new();
+    let url = format!(
+        "https://api.notion.com/v1/databases/{}/query",
+        credentials.id
+    );
+    let query_body = SimpleColumnQuery {
+        property: &column.name,
+    };
+
+    let response = client
+        .post(url)
+        .header("Authorization", format!("Bearer {}", credentials.token))
+        .header("Notion-Version", "2022-06-28")
+        .json(&query_body)
+        .send()
+        .await;
+
+    response_to_result(response.unwrap()).await
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let db = fetch_notion_database(&args.notion_db, &args.integration_secret).await?;
-    let rows = get_db_rows(&db)?;
-    println!("{rows:#?}");
+    let credentials = DatabaseCredentials {
+        id: args.notion_db,
+        token: args.integration_secret,
+    };
+    let db = fetch_notion_database(&credentials).await?;
+
+    let columns = get_db_columns(&db.body)?;
+
+    if columns.is_none() {
+        println!("No rows found");
+        return Ok(());
+    }
+
+    let columns = columns.unwrap();
+
+    println!("{:#?}", columns);
+
+    let email_col = columns.iter().find(|col| col.name == "Email");
+    if email_col.is_none() {
+        println!("No email column found");
+        return Ok(());
+    }
+
+    let row = query_rows(&credentials, email_col.unwrap()).await?;
+    println!("{}", row);
     Ok(())
 }
